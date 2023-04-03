fedb00905c2eb865bb469234ecea5ce4e4d72540
NathanH2111 <nathan@inhouseprinting.net>
2023-04-03 08:20:05 -0400

    commented out db connection

--- character builder/builderFunctions.php
+++ character builder/builderFunctions.php
@@ -1,22 +1,22 @@
 <?php
 
-$db_handle = pg_connect("host=localhost dbname=Dndbuilder user=williemdevenney password=password");
+// $db_handle = pg_connect("host=localhost dbname=Dndbuilder user=williemdevenney password=password");
 
-$query = "SELECT name, damagedice, damagetype, abilites
-FROM weapon
-WHERE quality='martial' AND distance='melee'";
+// $query = "SELECT name, damagedice, damagetype, abilites
+// FROM weapon
+// WHERE quality='martial' AND distance='melee'";
 
-$barbWeps1 = pg_fetch_all(pg_query($db_handle, $query));
+// $barbWeps1 = pg_fetch_all(pg_query($db_handle, $query));
 
-$query = "SELECT name, damagedice, damagetype, abilites
-FROM weapon
-WHERE quality='simple' AND distance='melee'";
+// $query = "SELECT name, damagedice, damagetype, abilites
+// FROM weapon
+// WHERE quality='simple' AND distance='melee'";
 
-$barbWeps2 = pg_fetch_all(pg_query($db_handle, $query));
+// $barbWeps2 = pg_fetch_all(pg_query($db_handle, $query));
 
-$query = "SELECT name, damagedice, damagetype, abilites
-FROM weapon
-WHERE name = 'Rapier' OR (quality='simple' AND distance='melee')";
+// $query = "SELECT name, damagedice, damagetype, abilites
+// FROM weapon
+// WHERE name = 'Rapier' OR (quality='simple' AND distance='melee')";
 
 
 
